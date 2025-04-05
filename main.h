85cca943a7146a6532efb52f1187e0a30990bf77
OkanMahmudov <mahmudovokan47@gmail.com>
2025-04-05 18:16:02 +0400

    main.h file

--- main.h
+++ main.h
@@ -1,13 +1,15 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
 #include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
 
 int _printf(const char *format, ...);
+void write_char(int *len, char c);
+void write_str(int *len, char *str);
+void write_int(int *len, int num);
 
-int main(void)
-{
-    int printed;
-
-    printed = _printf("Hello, %s! Your score is %c.\n", "Alice", 'A');
-    printf("Printed %d characters.\n", printed);
-
-    return 0;
-}
+#endif
